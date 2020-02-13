1323e9a29bbe7c593c6dbd9dd3b9a2e793a7e31a
secretmud@gmail.com <lamefeed@gmail.com>
2020-02-13 13:38:06 +0100

    Added time, you can now see how long it takes to execute the commands

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 void user_parse(char tmp[1024]);
 
@@ -10,12 +11,15 @@ int main(int argc, char * argv[]) {
     int i = 0;
     if (argc == 2) {
         file = fopen(argv[1], "r");
+        clock_t start;
+        start = clock();
         while(fgets(buffer, sizeof buffer, file) != NULL) {
             fputs(buffer, stdin);
             user_parse(buffer);
 	    i++;
         }
-	printf("%d users added\n", i);
+        start = clock() - start;
+        printf("%d users added in %.4fs\n", i, ((float)start)/ CLOCKS_PER_SEC);
         fclose(file);
     } else {
         printf("You need to pass a user file as a program argument\n");
@@ -42,8 +46,9 @@ void user_parse(char tmp[1024]) {
      * buffer[4] = HOME DIR
      * buffer[5] = SHELL
      * buffer[6] = PASSWORD
+     * sudo groupadd %s && sudo useradd
      **/
-    sprintf(cmd, "sudo groupadd %s && sudo useradd %s -u %s -p $(echo -n %s | openssl dgst -sha512 -binary | xxd -p) -G %s -c \"%s\" -d %s -s %s",
+    sprintf(cmd, "echo %s %s -u %s -p $(echo -n \"%s\" | openssl dgst -sha512 -binary | xxd -p) -G %s -c \"%s\" -d %s -s %s",
                         buffer[2], buffer[0], buffer[1], 
 			buffer[6], buffer[2], buffer[3],
                         buffer[4], buffer[5]);
