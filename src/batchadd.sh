98666c5b032f85a7a1875551370aa5796828ec45
Secretmud <lamefeed@gmail.com>
2020-02-13 15:40:22 +0100

    Update batchadd.sh
    Tested on Ubuntu 16.04

--- src/batchadd.sh
+++ src/batchadd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 while IFS=':' read -r name uid group user_comment home_dir shell pass; do
-    sudo groupadd $group && sudo useradd $name -u $uid -p $(openssl dgst -sha512 <<< $pass) -G $group -c $user_comment -d $home_dir -s $shell
+    sudo groupadd $group && sudo useradd $name -u $uid -p $(mkpasswd -m sha-512 $pass) -G $group -c "$user_comment" -d $home_dir -s $shell
 done < file.txt
